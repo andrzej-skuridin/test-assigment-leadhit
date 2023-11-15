71634ec519827a5193f37cfac42ab2b41f44f8c5
Andrzej Skuridin <andreyskuridin1995@gmail.com>
2023-11-15 23:06:33 +0300

    basic tests added

--- template_searcher/api/tests.py
+++ template_searcher/api/tests.py
@@ -1,3 +1,113 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class GetSingleFieldTemplateTestCase(APITestCase):
+
+    def test_get_only_email(self):
+        data = {'contact_email': 'user@mail.com'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"template_name": "Only email"}',
+            msg='Не тот шаблон')
+
+    def test_get_only_date(self):
+        data = {'registration_date': '12.12.1212'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"template_name": "Only date"}',
+            msg='Не тот шаблон')
+
+    def test_get_only_text(self):
+        data = {'bio': 'Some information'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"template_name": "Only text"}',
+            msg='Не тот шаблон')
+
+    def test_get_only_telephone(self):
+        data = {'telephone_number': '+79998887766'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"template_name": "Only telephone"}',
+            msg='Не тот шаблон')
+
+
+class IncorrectFormatTestCase(APITestCase):
+
+    def test_incorrect_email(self):
+        data = {'contact_email': 'not email format'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"contact_email": "not email format"}',
+            msg='Валидация не сработала.')
+
+    def test_incorrect_date(self):
+        data = {'registration_date': 'not date format'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"registration_date": "not date format"}',
+            msg='Валидация не сработала.')
+
+    def test_incorrect_telephone(self):
+        data = {'telephone_number': 'not telephone format'}
+        response = self.client.post(
+            path='/api/get_form/',
+            data=data,
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=status.HTTP_200_OK,
+            msg='HTTP статус не 200. Сервер поднят?')
+        self.assertEqual(
+            first=response.content,
+            second=b'{"telephone_number": "not telephone format"}',
+            msg='Валидация не сработала.')
--- template_searcher/api/views.py
+++ template_searcher/api/views.py
@@ -19,6 +19,6 @@ class GetFormAPIview(APIView):
         return HttpResponse('Hello, friend!')
 
     def post(self, request, **kwargs):
-        data = self.request.data  # dict
+        data = self.request.data
         found_template = db_handler(data)
         return JsonResponse(found_template)
